4b91f1a1e8dcf730c17ae01d702cfec6f89e0736
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2023-06-04 21:18:05 -0300

    application.properties update

--- src/main/java/com/bonatto/airline/controller/AircraftController.java
+++ src/main/java/com/bonatto/airline/controller/AircraftController.java
@@ -7,12 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.bonatto.airline.domain.airline.dto.AircraftDetailData;
@@ -64,6 +59,14 @@ public class AircraftController {
     			.map(AircraftDetailData :: new);
     	
     	return ResponseEntity.ok(page);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id)
+    {
+        aircraftRepo.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
 
