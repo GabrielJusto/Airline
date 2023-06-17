d4f4f6c3ed4200e2940537557cf819c87f732b2a
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2023-06-16 21:08:14 -0300

    list flghts

--- src/main/java/com/bonatto/airline/controller/FlightController.java
+++ src/main/java/com/bonatto/airline/controller/FlightController.java
@@ -2,15 +2,16 @@ package com.bonatto.airline.controller;
 
 import com.bonatto.airline.domain.airline.dto.FlightScheduleData;
 import com.bonatto.airline.domain.airline.dto.FlightScheduleDetailData;
+import com.bonatto.airline.domain.airline.repository.FlightRepository;
 import com.bonatto.airline.domain.airline.service.FlightScheduleService;
+import com.bonatto.airline.domain.customer.dto.CustomerData;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -22,10 +23,13 @@ public class FlightController {
 	
 	@Autowired
 	private FlightScheduleService flightService;
+
+	@Autowired
+	private FlightRepository flightRepo;
 	
-	@SuppressWarnings("rawtypes")
+
 	@PutMapping("/schedule")
-	public ResponseEntity scheduleFlight(@RequestBody @Valid FlightScheduleData fligh, UriComponentsBuilder uriBuilder)
+	public ResponseEntity<FlightScheduleDetailData> scheduleFlight(@RequestBody @Valid FlightScheduleData fligh, UriComponentsBuilder uriBuilder)
 	{
 		
 		FlightScheduleDetailData flightData = flightService.schedule(fligh);
@@ -34,6 +38,16 @@ public class FlightController {
 		URI uri = uriBuilder.path("/flight/{id}").buildAndExpand(flightData.id()).toUri();
         return ResponseEntity.created(uri).body(flightData);
         
+	}
+
+	@GetMapping("/list")
+	public ResponseEntity<Page<FlightScheduleDetailData>> list(Pageable pageable)
+	{
+		Page<FlightScheduleDetailData> page =
+				flightRepo.findAll(pageable)
+				.map(FlightScheduleDetailData :: new);
+		return ResponseEntity.ok(page);
+
 	}
 
 }
